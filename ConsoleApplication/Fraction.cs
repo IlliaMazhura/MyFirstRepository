4575a408e127d38f0ad35e6ab5734662c28e09b9
Илья О. Мажура <08370@zp.itstep.org>
2017-05-18 18:40:32 +0300

    Added 2 Public constructor

--- ConsoleApplication/Fraction.cs
+++ ConsoleApplication/Fraction.cs
@@ -6,11 +6,42 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
-    class Fraction
+    public struct Fraction : IEquatable<Fraction>
     {
-        private double a;
-        private double b { get; set; }
 
+
+        long _numerator;
         
+        public long Numerator
+        {
+            get { return _numerator; }
+            private set { _numerator = value; }
+        }
+
+
+
+        long _denominator;
+        
+        public long Denominator
+        {
+            get { return _denominator == 0 ? 1 : _denominator; }
+            private set
+            {
+                if (value == 0)
+                    throw new InvalidOperationException("Denominator cannot be assigned a 0 Value.");
+
+                _denominator = value;
+            }
+        }
+
+
+
+        public Fraction(long value)
+        {
+            _numerator = value;
+            _denominator = 1;
+            Reduce();
+        }
+
     }
 }
